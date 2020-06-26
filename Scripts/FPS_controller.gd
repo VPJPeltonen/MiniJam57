7c050775ebd8ffdc9f25c037f7d63d2c8c72d17f
VPJP <vpjpeltonen@gmail.com>
2020-06-26 16:38:38 +0300

    movement controls added

--- Scripts/FPS_controller.gd
+++ Scripts/FPS_controller.gd
@@ -1,6 +1,12 @@
 extends KinematicBody
 
-var mouse_sens = 0.3
+export var speed = 10
+export var acceleration = 5
+export var gravity = 0.98
+export var jump_power = 30
+export var mouse_sens = 0.3
+var velocity = Vector3()
+
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -14,3 +20,27 @@ func _input(event):
 		var x_rot = -event.relative.y * mouse_sens
 		x_rot = clamp(x_rot,-90,90)
 		$Head/Camera.rotate_x(deg2rad(x_rot))
+
+func _physics_process(delta):
+	var head_basis = $Head.get_global_transform().basis
+	
+	var direction = Vector3()
+	if Input.is_action_pressed("move_forward"):
+		direction -= head_basis.z
+	elif Input.is_action_pressed("move_backward"):
+		direction += head_basis.z
+	
+	if Input.is_action_pressed("move_left"):
+		direction -= head_basis.x
+	elif Input.is_action_pressed("move_right"):
+		direction += head_basis.x
+	
+	direction = direction.normalized()
+	
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	velocity.y -= gravity
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += jump_power
+	
+	velocity = move_and_slide(velocity, Vector3.UP)
