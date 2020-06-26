43cba059d829668bc40f1d3369c487614f79e17a
VPJP <vpjpeltonen@gmail.com>
2020-06-26 18:36:51 +0300

    new version of selection outline

--- Scripts/test_cube.gd
+++ Scripts/test_cube.gd
@@ -2,15 +2,11 @@ extends StaticBody
 
 var invisible = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func show_activetable():
-	$MeshInstance.get_surface_material(0).rim_enabled = true
+	$MeshInstance/Outline.show()
 
 func hide_activetable():
-	$MeshInstance.get_surface_material(0).rim_enabled = false
+	$MeshInstance/Outline.hide()
 
 func player_activate():
 	if invisible:
