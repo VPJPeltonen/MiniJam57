baf4e5f9faec550ffadaf8b4117d2c662808f941
VPJP <vpjpeltonen@gmail.com>
2020-06-27 16:50:30 +0300

    fixed info display on server

--- Scripts/Analyzer.gd
+++ Scripts/Analyzer.gd
@@ -5,4 +5,4 @@ func show_info(item):
 		show()
 		$InfoBox/ColorRect/Label.text = item.info
 		#move the infobox to screen position of object being analyzed
-		$InfoBox.rect_position = get_parent().get_node("Head/Camera").unproject_position(item.translation)
+		$InfoBox.rect_position = get_parent().get_node("Head/Camera").unproject_position(item.transform.origin)
