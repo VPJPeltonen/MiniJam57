57d0c9c764d3cfcda0b660aeb3f77ebdb70c6617
VPJP <vpjpeltonen@gmail.com>
2020-06-27 22:58:31 +0300

    added pickable object

---
+++ Scripts/pickable_object.gd
@@ -0,0 +1,34 @@
+extends Spatial
+
+export var info = "This appears to be a cube"
+export var has_item = false
+export var item_held = "none" 
+
+var picked_up = false
+
+var outline_mesh: MeshInstance
+
+func _ready():
+	var path := "MeshInstance/Outline"
+	var path2 := "Outline"
+	if has_node(path):
+		outline_mesh = get_node(path)
+	elif has_node(path2):
+		outline_mesh = get_node(path2)
+	else:
+		push_error("Could not find Outline Mesh for %s" % name)
+	
+	outline_mesh.hide()
+
+func show_activetable():
+	if picked_up:
+		return
+	outline_mesh.show()
+
+func hide_activetable():
+	outline_mesh.hide()
+
+func player_activate(player):
+	$MeshInstance.hide()
+	outline_mesh.hide()
+	picked_up = true
