11a3c2ac0e83f31c44691f63261894be2e925892
Paspartout <Paspartout@users.noreply.github.com>
2020-06-27 13:38:12 +0200

    Add info object to time machine object.
    Make text wrap in Analyzer, try out lowrez rendering

--- Scripts/info_object.gd
+++ Scripts/info_object.gd
@@ -1,12 +1,26 @@
-extends StaticBody
+extends Spatial
 
 export var info = "This appears to be a cube"
 
+var outline_mesh: MeshInstance
+
+func _ready():
+	var path := "MeshInstance/Outline"
+	var path2 := "Outline"
+	if has_node(path):
+		outline_mesh = get_node(path)
+	elif has_node(path2):
+		outline_mesh = get_node(path2)
+	else:
+		push_error("Could not find Outline Mesh for %s" % name)
+	
+	outline_mesh.hide()
+
 func show_activetable():
-	$MeshInstance/Outline.show()
+	outline_mesh.show()
 
 func hide_activetable():
-	$MeshInstance/Outline.hide()
+	outline_mesh.hide()
 
 func player_activate():
 	pass
