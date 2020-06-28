28ddd7397868433f9fc4e8280d9c2e9abc784d74
VPJP <vpjpeltonen@gmail.com>
2020-06-28 23:31:03 +0300

    inventory list fixed

--- Scripts/InventoryUI.gd
+++ Scripts/InventoryUI.gd
@@ -3,5 +3,5 @@ extends Node2D
 func update_inventory(items):
 	var inventory = "Inventory:\n"
 	for item in items:
-		inventory += item + "\b"
+		inventory += item + "\n"
 	$InfoBox2/ColorRect/Label.text = inventory
